2b8c9945e6f4a854ad8f40e40504b02e6eb4341d
Tracy Chang <sd108tracy@vfs.com>
2025-12-07 16:47:31 +0800

    menu botton click

---
+++ WitchesDontBurn/Assets/Scripts/UIButtonSound.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class UIButtonSound : MonoBehaviour
+{
+    [SerializeField] private AK.Wwise.Event _uiWwiseEvent = null;
+
+    public void Play()
+    {
+        _uiWwiseEvent.Post(gameObject);
+    }
+}
